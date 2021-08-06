4092d2e9f5cf61a0cb865674a1afa57fdd3f580d
Toxidation2 <81825067+Toxidation2@users.noreply.github.com>
2021-08-05 20:38:13 -0400

    Update index.py

--- htdocs/index.py
+++ htdocs/index.py
@@ -1,18 +1,18 @@
 print("""<!doctype html>
-<html lang=\"en\">")
-     <head>")
-         <meta charset=\"utf-8\">")
-         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">")
-         <title>Hello world!</title>")
-         <style>")
-             html {")
-                 font-family: Verdana, Geneva, Tahoma, sans-serif;")
-                 text-align: center;")
-             }")
-         </style>")
-     </head>")
-     <body>")
-         <h1>Hello world!</h1>")
-         <p>Hello world!</p>")
-     </body>")
+<html lang=\"en\">
+     <head>
+         <meta charset=\"utf-8\">
+         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+         <title>Hello world!</title>
+         <style>
+             html {
+                 font-family: Verdana, Geneva, Tahoma, sans-serif;
+                 text-align: center;
+             }
+         </style>
+     </head>
+     <body>
+         <h1>Hello world!</h1>
+         <p>Hello world!</p>
+     </body>
 </html>""")
